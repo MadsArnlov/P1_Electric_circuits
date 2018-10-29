88cc4de0e8c7b68ffbbf275a06e8dc51e34d9a05
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2018-10-29 13:00:17 +0100

    Plot af opladning

--- Python_kode.py
+++ Python_kode.py
@@ -14,22 +14,30 @@ C = 98.98 * 10**(-9)
 tau = R*C
 V0 = 2
 
-t = np.linspace(0, 10*tau, 1000)
+V = 4
+
+t_vc  = np.linspace(0, 10*tau, 1000)
 
 V_C = np.exp(-t/tau)*V0 - V0/2
+
+V_t = V0 + (V0 - V)*np.exp(-t/tau) - V0/2
+
+t_vt = np.linspace(10*tau, 20*tau, 1000)
 
 # =============================================================================
 # Square wave
 # =============================================================================
 
-sqwave = np.piecewise(t, [t == 0, t > 0], [V0/2, -V0/2])
+t_sqwave = np.linspace(0, 20*tau, 1000)
+
+sqwave = np.piecewise(t, [t > 10*tau, t < 10*tau, t == 0], [V0/2, -V0/2, V0/2])
 
 # =============================================================================
 # Plotting
 # =============================================================================
 
 plt.figure(figsize = (12, 8))
-plt.plot(t, V_C, 'r-', t, sqwave, 'b-')
+plt.plot(t_vc, V_C, 'r-', t_sqwave, sqwave, 'b-', t_vt, V_t, 'g-')
 plt.xlabel('$t$ [s]')
 plt.ylabel('$V_C$ [V]')
 plt.title('Voltage across capacitor')
