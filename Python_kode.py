4344f47dafabeb918a4dd721cc861c9b60609663
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2018-10-29 15:50:43 +0100

    Update Python_kode.py

--- Python_kode.py
+++ Python_kode.py
@@ -6,6 +6,16 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 # =============================================================================
+# Data
+# =============================================================================
+
+data=np.genfromtxt("forsøgsdata.csv", delimiter=",")
+
+tid = data[1:,0]
+sqwave_data = data[1:,1]
+cap = data[1:,2]
+
+# =============================================================================
 # Voltage drop across capacitor as a function of time
 # =============================================================================
 
@@ -16,11 +26,13 @@ V0 = 2
 
 V = 4
 
-t_vc  = np.linspace(0, 5*tau, 1000)
+time_charge = (4*10**-6 - tid[0])
+
+t_vc  = np.linspace(0, time_charge, 5000)
 
 V_C = np.exp(-t_vc/tau)*V0 - V0/2
 
-t_vt = np.linspace(0*tau, 5*tau, 1000)
+t_vt = np.linspace(0*tau, time_charge, 5000)
 
 V_t = V0 + (V0 - V)*np.exp(-t_vt/tau) - V0/2
 
@@ -28,10 +40,10 @@ V_t = V0 + (V0 - V)*np.exp(-t_vt/tau) - V0/2
 # Square wave
 # =============================================================================
 
-t_sqwave = np.linspace(0, 10*tau, 1000)
+t_sqwave = np.linspace(0, time_charge*2, 1000)
 
 sqwave = np.piecewise(t_sqwave, 
-                      [t_sqwave > 5*tau, t_sqwave < 5*tau, t_sqwave == 0], 
+                      [t_sqwave > time_charge, t_sqwave < time_charge, t_sqwave == 0], 
                       [V0/2, -V0/2, V0/2])
 
 # =============================================================================
@@ -39,8 +51,13 @@ sqwave = np.piecewise(t_sqwave,
 # =============================================================================
 
 plt.figure(figsize = (12, 8))
-plt.plot(t_vc, V_C, 'r-', t_sqwave, sqwave, 'b-', 5*tau + t_vt, V_t, 'g-')
+plt.plot(tid - tid[0], cap, 'k,', t_vc, V_C, 'r-', 
+         t_sqwave, sqwave, 'g-', time_charge + t_vt, V_t, 'b-')
 plt.xlabel('$t$ [s]')
 plt.ylabel('$V_C$ [V]')
 plt.title('Voltage across capacitor')
 # plt.savefig('Afladning_kapacitor.pdf')
+
+plt.figure(figsize = (12, 8))
+plt.plot(t_vc, cap[:5000] - V_C, 'k-', 
+         time_charge + t_vt, cap[5000:] - V_t, 'k-')
