e6ae25365aa54a9388456de2b9edf5025da78df3
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2018-11-19 18:38:32 +0100

    bodeplot

--- Python_kode.py
+++ Python_kode.py
@@ -4,26 +4,41 @@
 "The program uses the modules matplotlib and numpy"
 import matplotlib.pyplot as plt
 import numpy as np
+import sys
 
 # =============================================================================
 # Data
 # =============================================================================
 "The data is exported from 'Waveform'."
-data = np.genfromtxt("forsøgsdata.csv", delimiter=",")
+"Voltage across capacitor in time domain"
+data_time = np.genfromtxt("forsøgsdata.csv", delimiter=",")
 
-time = data[1:, 0]
-sqwave_data = data[1:, 1]
-cap = data[1:, 2]
+time = data_time[1:, 0]
+sqwave_data = data_time[1:, 1]
+cap = data_time[1:, 2]
 
 "'time_charge' is the measured time before the capacitor starts charging."
 time_charge = (time[5001] - time[0])
+
+"Magnitude of capacitor/resistor in frequency domain"
+data_magnitude_C = np.genfromtxt("bode_low_1v4753ohm96_73nf.csv", delimiter=",")
+
+frequency_C = data_magnitude_C[1:, 0]
+magnitude_C = data_magnitude_C[1:, 2]
+phase_C = data_magnitude_C[1:, 3]
+
+data_magnitude_R = np.genfromtxt("bode_high_1v4753ohm96_73nf.csv", delimiter=",")
+
+frequency_R = data_magnitude_R[1:, 0]
+magnitude_R = data_magnitude_R[1:, 2]
+phase_R = data_magnitude_R[1:, 3]
 
 # =============================================================================
 # Voltage drop across capacitor as a function of time
 # =============================================================================
 "The values of the components used in the experiment are defined:"
-R = 4732                                            # Resistance
-C = 98.98 * 10**(-9)                                # Capacitance
+R = 4753                                            # Resistance
+C = 96.73 * 10**(-9)                                # Capacitance
 tau = R*C                                           # Time constant
 omega_c = 1/tau                                     # Cutoff frequency
 V0 = 2                                              # Initial voltage
@@ -38,7 +53,7 @@ an arary for time is made, t, which has the length of 5000, uniform spaced
 values between 0 and 'time_charge'.
 """
 t = np.linspace(0, time_charge, 5000)
-omega = np.linspace(1, 100/tau, 5000)
+omega = np.linspace(1, 100000, 5000)
 
 
 def V_C(t):
@@ -160,130 +175,115 @@ sqwave = np.piecewise(t_sqwave,                 # The parameter
 # =============================================================================
 # Plot of data
 # =============================================================================
-
-plt.figure(figsize=(12, 8))
-plt.plot(time - time[0], cap, 'b,')
-plt.xlabel('$t$ [s]')
-plt.ylabel('$V_C$ [V]')
-plt.title('Data of RC-circuit')
-plt.savefig('data.png')
+if len(sys.argv) == 2:
+    if sys.argv[1] == 'data':
+        plt.figure(figsize=(12, 8))
+        plt.plot(time - time[0], cap, 'b,')
+        plt.xlabel('$t$ [s]')
+        plt.ylabel('$V_C$ [V]')
+        plt.title('Data of RC-circuit')
+        plt.savefig('data.png')
 
 # =============================================================================
 # Plot of mathematical model
 # =============================================================================
 
-plt.figure(figsize=(12, 8))
-plt.plot(t, V_C(t), 'tab:orange')
-plt.plot(time_charge + t, V_C2(t), 'tab:orange')
-plt.xlabel('$t$ [s]')
-plt.ylabel('$V_C$ [V]')
-plt.title('Mathematical model')
-plt.savefig('mathematical_model.png')
+    if sys.argv[1] == 'model':
+        plt.figure(figsize=(12, 8))
+        plt.plot(t, V_C(t), 'tab:orange')
+        plt.plot(time_charge + t, V_C2(t), 'tab:orange')
+        plt.xlabel('$t$ [s]')
+        plt.ylabel('$V_C$ [V]')
+        plt.title('Mathematical model')
+        plt.savefig('mathematical_model.png')
 
 # =============================================================================
 # Plot of data and mathematical model
 # =============================================================================
 
-plt.figure(figsize=(12, 8))
-plt.plot(t, V_C(t), 'tab:orange', label='Modelled voltage')
-plt.plot(time_charge + t, V_C2(t), 'tab:orange')
-plt.plot(time - time[0], cap, 'b,', label='Data points')
-plt.plot(tau, V_C(tau), 'kx', label='$V_C(tau)$')
-plt.plot(t_sqwave, sqwave, 'k--', label='Square wave')
-plt.xlabel('$t$ [s]')
-plt.ylabel('$V_C$ [V]')
-plt.legend()
-plt.title('Voltage across capacitor')
-plt.savefig('data_vs_model.png')
+    if sys.argv[1] == 'data_vs_model':
+        plt.figure(figsize=(12, 8))
+        plt.plot(t, V_C(t), 'tab:orange', label='Modelled voltage')
+        plt.plot(time_charge + t, V_C2(t), 'tab:orange')
+        plt.plot(time - time[0], cap, 'b,', label='Data points')
+        plt.plot(tau, V_C(tau), 'kx', label='$V_C(tau)$')
+        plt.plot(t_sqwave, sqwave, 'k--', label='Square wave')
+        plt.xlabel('$t$ [s]')
+        plt.ylabel('$V_C$ [V]')
+        plt.legend()
+        plt.title('Voltage across capacitor')
+        plt.savefig('data_vs_model.png')
 
 # =============================================================================
 # Plot of deviation between data and mathematical model
 # =============================================================================
 
-plt.figure(figsize=(12, 8))
-plt.plot(t, cap[:5000] - V_C(t), 'k,',
-         time_charge + t, cap[5000:] - V_C2(t), 'k,')
-plt.xlabel('t [s]')
-plt.ylabel('$V_{data} - V_C$ [V]')
-plt.title('Difference between data and model')
-plt.savefig('deviation.png')
+    if sys.argv[1] == 'deviation':
+        plt.figure(figsize=(12, 8))
+        plt.plot(t, cap[:5000] - V_C(t), 'k,',
+                 time_charge + t, cap[5000:] - V_C2(t), 'k,')
+        plt.xlabel('t [s]')
+        plt.ylabel('$V_{data} - V_C$ [V]')
+        plt.title('Difference between data and model')
+        plt.savefig('deviation.png')
 
 # =============================================================================
 # Bodeplot of RC LP filter
 # =============================================================================
 
-plt.figure(figsize=(12, 8))
-plt.subplot(2, 1, 1)
-plt.semilogx(omega, 20*np.log10(H_lp(omega)),
-             'b-', label='LP Transfer function')
-plt.semilogx(omega_c, 20*np.log10(H_lp(omega_c)),
-             'kx', label='Gain at $\omega=\omega_c$')
-plt.legend()
-plt.grid(True)
-plt.ylabel('Magnitude $G(j\omega)$ [dB]')
-
-
-plt.subplot(2, 1, 2)
-plt.semilogx(omega, np.arctan(-omega*tau)*180/np.pi,
-             'b-', label='LP Phase shift')
-plt.semilogx(omega_c, np.arctan(-omega_c*tau)*180/np.pi,
-             'kx', label='Phase at $\omega=\omega_c$')
-plt.yticks(np.arange(0, -105, step=-15))
-plt.legend()
-plt.grid(True)
-plt.xlabel('Angular frequency $\omega$ [Hz]')
-plt.ylabel('Phase $\u03B8$ [degrees]')
-
-plt.savefig('bodeplot_rc_lp.png')
-plt.show()
+    if sys.argv[1] == 'RCLP':
+        plt.figure(figsize=(12, 8))
+        plt.subplot(2, 1, 1)
+        plt.semilogx(omega, 20*np.log10(H_lp(omega)),
+                     'b-', label='LP Transfer function')
+        plt.semilogx(omega_c, 20*np.log10(H_lp(omega_c)),
+                     'kx', label='Gain at $\omega=\omega_c$')
+        plt.plot(frequency_C, magnitude_C,
+                     'r-', label='Data')
+        plt.legend()
+        plt.grid(True)
+        plt.ylabel('Magnitude $G(j\omega)$ [dB]')
+        
+        
+        plt.subplot(2, 1, 2)
+        plt.semilogx(omega, np.arctan(-omega*tau)*180/np.pi,
+                     'b-', label='LP Phase shift')
+        plt.semilogx(omega_c, np.arctan(-omega_c*tau)*180/np.pi,
+                     'kx', label='Phase at $\omega=\omega_c$')
+        plt.yticks(np.arange(0, -105, step=-15))
+        plt.legend()
+        plt.grid(True)
+        plt.xlabel('Angular frequency $\omega$ [Hz]')
+        plt.ylabel('Phase $\u03B8$ [degrees]')
+        
+        plt.savefig('bodeplot_rc_lp.png')
+        plt.show()
 
 # =============================================================================
 # Bodeplot of RC HP filter
 # =============================================================================
 
-plt.figure(figsize=(12, 8))
-plt.subplot(2, 1, 1)
-plt.semilogx(omega, 20*np.log10(H_hp(omega)),
-             'b-', label='HP Transfer function')
-plt.semilogx(1/tau, 20*np.log10(H_hp(1/tau)),
-             'kx', label='Gain at time=1/tau')
-plt.legend()
-plt.grid(True)
-plt.ylabel('Magnitude $G(j\omega)$ [dB]')
-
-plt.subplot(2, 1, 2)
-plt.semilogx(omega, np.arctan(1/(omega*tau))*180/np.pi,
-             'b-', label='HP Phase shift')
-plt.yticks(np.arange(0, 105, step=15))
-plt.legend()
-plt.grid(True)
-plt.xlabel('Angular frequency $\omega$ [Hz]')
-plt.ylabel('Phase $\u03B8$ [degree]')
-
-plt.savefig('bodeplot_rc_hp.png')
-plt.show()
-
-# =============================================================================
-# Bodeplot of RC BP filter
-# =============================================================================
-
-plt.figure(figsize=(12, 8))
-#plt.subplot(2, 1, 1)
-plt.semilogx(omega, 20*np.log10(H_bp(omega)),
-             'b-', label='BP Transfer function')
-plt.semilogx(1/tau, 20*np.log10(H_bp(1/tau)),
-             'kx', label='Gain at time=1/tau')
-plt.semilogx(1/tau2, 20*np.log10(H_bp(1/tau2)),
-             'kx')
-plt.legend()
-plt.ylabel('Magnitude [dB]')
-plt.title('Bodeplot of RC BP filter')
-
-#plt.subplot(2, 1, 2)
-#plt.semilogx(s, np.arctan(s*tau)*180/np.pi, 'b-', label='HP Phase shift')
-#plt.yticks(np.arange(0, 105, step=15))
-#plt.legend()
-#plt.xlabel('Frequency [Hz]')
-#plt.ylabel('Phase [degree]')
-
-plt.show()
+    if sys.argv[1] == 'RCHP':
+        plt.figure(figsize=(12, 8))
+        plt.subplot(2, 1, 1)
+        plt.semilogx(omega, 20*np.log10(H_hp(omega)),
+                     'b-', label='HP Transfer function')
+        plt.semilogx(1/tau, 20*np.log10(H_hp(1/tau)),
+                     'kx', label='Gain at time=1/tau')
+        plt.plot(frequency_R, magnitude_R,
+                     'r-', label='Data')
+        plt.legend()
+        plt.grid(True)
+        plt.ylabel('Magnitude $G(j\omega)$ [dB]')
+        
+        plt.subplot(2, 1, 2)
+        plt.semilogx(omega, np.arctan(1/(omega*tau))*180/np.pi,
+                     'b-', label='HP Phase shift')
+        plt.yticks(np.arange(0, 105, step=15))
+        plt.legend()
+        plt.grid(True)
+        plt.xlabel('Angular frequency $\omega$ [Hz]')
+        plt.ylabel('Phase $\u03B8$ [degree]')
+        
+        plt.savefig('bodeplot_rc_hp.png')
+        plt.show()
