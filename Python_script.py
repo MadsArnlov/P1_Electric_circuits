1472180fc4436db15fdc4d4603af3cfa39234a9f
Mads Arnløv Jørgensen <43960127+MadsArnlov@users.noreply.github.com>
2018-12-07 09:15:28 +0100

    godt

--- Python_script.py
+++ Python_script.py
@@ -291,28 +291,28 @@ def deviation():
 def relative():
     discharging = ((cap[:5000] - V_C(t))*100)/abs(V_C(t))
     charging = ((cap[5000:] - V_C2(t))*100)/abs(V_C2(t))
-    for value in range(len(discharging)):
-        if discharging[value] >= 100:
-            discharging[value] = 0
-        elif discharging[value] <= -100:
-            discharging[value] = 0
-    for value in range(len(charging)):
-        if charging[value] <= -100:
-            charging[value] = 0
-        elif charging[value] >= 100:
-            charging[value] = 0
+#    for value in range(len(discharging)):
+#        if discharging[value] >= 100:
+#            discharging[value] = 0
+#        elif discharging[value] <= -100:
+#            discharging[value] = 0
+#    for value in range(len(charging)):
+#        if charging[value] <= -100:
+#            charging[value] = 0
+#        elif charging[value] >= 100:
+#            charging[value] = 0
     average_discharging = sum(abs(discharging)/len(discharging))
     average_charging = sum(abs(charging)/len(charging))
     print("The relative percentage difference of", "\n",
           "Discharging: {:.5f}%".format(average_discharging), "\n",
           "Charging: {:.5f}%".format(average_charging))
     plt.figure(figsize=(12, 8))
-    plt.subplot(2, 1, 1)
-    plt.plot(t, cap[:5000] - V_C(t), 'k,',
-             abs(time[0]) + t, cap[5000:] - V_C2(t), 'k,')
-    plt.ylabel('$V_{data} - V_C$ [V]')
-
-    plt.subplot(2, 1, 2)
+#    plt.subplot(2, 1, 1)
+#    plt.plot(t, cap[:5000] - V_C(t), 'k,',
+#             abs(time[0]) + t, cap[5000:] - V_C2(t), 'k,')
+#    plt.ylabel('$V_{data} - V_C$ [V]')
+#
+#    plt.subplot(2, 1, 2)
     plt.plot(t, discharging, 'k,',
              abs(time[0]) + t, charging, 'k,')
     plt.ylabel('% Difference')
